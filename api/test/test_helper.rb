006e0b8618faa474d06ba8c5f5a94136fe1032a8
kei-600th <kei19941005@gmail.com>
2023-05-15 16:08:20 +0900

    Railsユーザーモデルバリデーションテスティング(name/email/password)

--- api/test/test_helper.rb
+++ api/test/test_helper.rb
@@ -10,7 +10,7 @@ class ActiveSupport::TestCase
   parallelize_setup do |worker|
     load "#{Rails.root}/db/seeds.rb"
   end
-  
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
@@ -18,4 +18,7 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def active_user
+    User.find_by(activated: true)
+  end
 end
