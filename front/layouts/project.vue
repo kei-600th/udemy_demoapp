688ea24900bdf92b09024e4bc0c77665c866cffd
kei-600th <kei19941005@gmail.com>
2023-05-17 05:52:09 +0900

    動的なルートに表示する個別プロジェクトページを作成する

---
+++ front/layouts/project.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <logged-in-app-bar />
+    <v-main>
+      project.vue
+      <nuxt />
+    </v-main>
+    <app-footer />
+  </v-app>
+</template>
+
+<script>
+export default {
+}
+</script>
