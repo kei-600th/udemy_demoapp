3160280d2b650981fd1e3528b742c9b0abc4f790
kei-600th <kei19941005@gmail.com>
2023-05-16 16:05:57 +0900

    ログインページの作成

--- front/pages/login.vue
+++ front/pages/login.vue
@@ -1,11 +1,63 @@
 <template>
-  <div>
-    login.vue
-  </div>
+  <user-form-card>
+    <template
+      #user-form-card-content
+    >
+      <v-form
+      ref="form"
+      v-model="isValid"
+      >
+        <user-form-email
+          :email.sync="params.user.email"
+        />
+        <user-form-password
+          :password.sync="params.user.password"
+        />
+        <v-card-actions>
+          <nuxt-link
+            to="#"
+            class="body-2 text-decoration-none"
+          >
+            パスワードを忘れた?
+          </nuxt-link>
+        </v-card-actions>
+        <v-card-text
+        class="px-0"
+        >
+          <v-btn
+            :disabled="!isValid || loading"
+            :loading="loading"
+            block
+            color="appblue"
+            class="white--text"
+            @click="login"
+          >
+            ログインする
+            </v-btn>
+        </v-card-text>
+      </v-form>
+      <v-card-text>
+      </v-card-text>
+    </template>
+  </user-form-card>
 </template>
 
 <script>
+
 export default {
-  layout: 'beforeLogin'
+  layout: 'beforeLogin',
+  data () {
+    return {
+      isValid: false,
+      loading: false,
+      params: { user: { email: '', password: '' } }
+    }
+  },
+  methods: {
+    login () {
+      this.loading = true
+      setTimeout(() => (this.loading = false), 1500)
+    }
+  }
 }
 </script>
--- front/pages/signup.vue
+++ front/pages/signup.vue
@@ -4,6 +4,7 @@
       #user-form-card-content
     >
       <v-form
+      ref="form"
       v-model="isValid"
       >
         <user-form-name
@@ -18,16 +19,17 @@
           set-validation
         />
         <v-btn
-        :disabled="!isValid"
+        :disabled="!isValid || loading"
+        :loading="loading"
         block
         color="appblue"
         class="white--text"
+        @click="signup"
       >
         登録する
         </v-btn>
       </v-form>
       <v-card-text>
-        {{ params }}
       </v-card-text>
     </template>
   </user-form-card>
@@ -40,7 +42,23 @@ export default {
   data () {
     return {
       isValid: false,
+      loading: false,
       params: { user: { name: '', email: '', password: '' } }
+    }
+  },
+  methods: {
+    signup () {
+      this.loading = true
+      setTimeout(() => {
+        this.formReset()
+        this.loading = false
+      }, 1500)
+    },
+    formReset () {
+      this.$refs.form.reset()
+      for (const key in this.params.user) {
+        this.params.user[key] = ''
+      }
     }
   }
 }
