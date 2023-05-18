dda388ce7d8bb1c862c678067a9e57cf5688e1d6
kei-600th <kei19941005@gmail.com>
2023-05-18 18:50:52 +0900

    アクセストークンからcurrent_userを取得させる

--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::UsersController < ApplicationController
 
+  before_action :authenticate_user
+
   def index
-    users = User.all
-    render json: users.as_json(only: [:id, :name, :email, :created_at])
+    # users = User.all
+    render json: current_user.as_json(only: [:id, :name, :email, :created_at])
   end
 
   
